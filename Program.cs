afa44a687fddacbd0874881d06767934c716e1da
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-23 14:10:54 +0200

    färdig med uppgift 2

--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hejsan skriv ditt förnamn:");
-            string namn = Console.ReadLine();
-
-            Console.WriteLine("Skriv in din favoritfärg:");
-            string färg = Console.ReadLine();
-
-            Console.WriteLine($"Ditt roliga namn är: Big {namn} av {färg} Tokyo");
+        //Skriva ett program(Console App)
+        //som frågar användare att först skriva in 2 tal i terminalen, för att
+        //programet ska addera de och skriva
+        //ut resultatet i terminal
+        Console.WriteLine("Skriv in första talet: ");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in andra talet: ");
+            int tal2 = int.Parse(Console.ReadLine());
+            int summa = tal1 + tal2;
+            Console.WriteLine($"Summan av {tal1} och {tal2} är {summa}");
         }
     }
 }
