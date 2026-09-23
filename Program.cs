bd780247969a0680438000a0994dc5f1d1de9f70
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-23 13:03:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Namngenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
