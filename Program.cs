eb9acb86ef40e21fa2afc3e17cca788875b2ea8f
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-23 14:16:39 +0200

    klar med uppgift 3

--- Program.cs
+++ Program.cs
@@ -4,16 +4,31 @@
     {
         static void Main(string[] args)
         {
-        //Skriva ett program(Console App)
-        //som frågar användare att först skriva in 2 tal i terminalen, för att
-        //programet ska addera de och skriva
-        //ut resultatet i terminal
-        Console.WriteLine("Skriv in första talet: ");
-            int tal1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Skriv in andra talet: ");
-            int tal2 = int.Parse(Console.ReadLine());
-            int summa = tal1 + tal2;
-            Console.WriteLine($"Summan av {tal1} och {tal2} är {summa}");
+            //Uppgift 3 – Pizzakalkylatorn
+            //Du ska räkna ut priset på pizzor med moms.
+            //Instruktioner:
+            //Skapa en const som heter MOMS och sätt den till 0.12(12 %).
+            const double MOMS = 0.12;
+            //Be användaren skriva in antal pizzor.
+            Console.WriteLine("Ange antal pizzor: ");
+            int antalPizzor = int.Parse(Console.ReadLine());
+            //Be användaren skriva in pris per pizza.
+            Console.WriteLine("Ange pris per pizza: ");
+            double prisPerPizza = double.Parse(Console.ReadLine());
+            //Räkna ut:
+            //Totalsumma utan moms
+            String totalsummaUtanMoms = (antalPizzor * prisPerPizza).ToString();
+            Console.WriteLine("Totalsumma utan moms: " + totalsummaUtanMoms);
+            //Momsbelopp
+            String momsbelopp = (antalPizzor * prisPerPizza * MOMS).ToString();
+            Console.WriteLine("Momsbelopp: " + momsbelopp);
+            //Totalsumma med moms
+            String totalsummaMedMoms = (antalPizzor * prisPerPizza + double.Parse(momsbelopp)).ToString();
+            Console.WriteLine("Totalsumma med moms: " + totalsummaMedMoms);
+            //Skriv ut alla tre resultaten på skärmen.
+            //Tips:
+            //Använd Convert.ToInt32() eller Convert.ToDouble() för att konvertera input.
+
         }
     }
 }
